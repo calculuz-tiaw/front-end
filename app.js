fe9543aa735267fc86054dbe81b111c3854ab1c1
Guilherme Coelho <guilherme.coelhofb@gmail.com>
2021-11-11 20:30:43 -0300

    using js file to login

--- app.js
+++ app.js
@@ -9,6 +9,41 @@ btnSignin.addEventListener("click", function () {
 
 btnSignup.addEventListener("click", function () {
   body.className = "sign-up-js";
+  signUp();
 });
 
-//teste branch feature/auth-login
+var btnRegister = document.querySelector("#registerBtn");
+var btnLogin = document.querySelector("#loginBtn");
+
+btnRegister.addEventListener("click", function () {
+  signUp();
+});
+
+btnLogin.addEventListener("click", function () {
+  signIn();
+});
+
+function signIn() {
+  let emailEl = document.querySelector("#email");
+  let passwordEl = document.querySelector("#senha");
+
+  let storedEmail = localStorage.getItem("email");
+  let storedPassword = localStorage.getItem("password");
+
+  // check if stored data from register-form is equal to data from login form
+  if (emailEl.value == storedEmail && passwordEl.value == storedPassword) {
+    alert("You are logged in.");
+    window.location.href = "http://www.devmedia.com.br";
+  } else {
+    alert("ERROR.");
+  }
+}
+
+function signUp() {
+  let emailEl = document.querySelector("#email2");
+  let passwordEl = document.querySelector("#senha2");
+  let nameEl = document.querySelector("#nome");
+  localStorage.setItem("email", emailEl.value);
+  localStorage.setItem("password", passwordEl.value);
+  localStorage.setItem("username", nameEl.value);
+}
